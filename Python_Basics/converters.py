6509cd3fc512836cb4fb0b820d0aa7fedb99e11e
Dateenbee <ahmaddatin98@gmail.com>
2026-08-06 16:41:13 +0800

    another

---
+++ Python_Basics/converters.py
@@ -0,0 +1,39 @@
+
+def find_max(x):
+    high = x[0]
+    for number in x:
+        if number > high:
+            high = number
+    return high
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def kg_to_lbs(x):
+#     convert = x * 2.204
+#     return convert
+
+# def lbs_to_kg(y):
+#     convert = y // 2.204  
+#     return convert
